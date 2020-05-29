1682b64a899a1a63dba3969662d747e1ee27bb8c
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-29 17:43:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(array) # put argument(s) here
   counter = 0
   while counter < array.length
-    # yield array[counter]
+    yield array[counter]
     counter += 1
   end
   # does something here with the elements of an array
