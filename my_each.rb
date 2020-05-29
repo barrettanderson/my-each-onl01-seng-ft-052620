8b1172f46021d2540d163f2b93a6e0db3e14f5cc
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-29 17:37:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,27 +1,27 @@
 def my_each(words) # put argument(s) here
   counter = 0
   while counter < array.length
-
-      counter += 1
+    # yield array[counter]
+    # counter += 1
   end
   # does something here with the elements of an array
   # returns the original array
-
+  
 end
 
 # my_each(collection) do |i|
 # puts i
 # end
 
-# Note: Some of the tests may be passing before you write any code. But, in
+# Note: Some of the tests may be passing before you write any code. But, in 
 # order to get all of the tests passing you'll need to use the yield keyword correctly.
-#
-# Now that you know how the yield method works, try to write your own version
+# 
+# Now that you know how the yield method works, try to write your own version 
 # of the #each method without using the #each method provided by Ruby.
-#
-# You'll be building out the content of the #my_each method.
-# This method should accept an argument of an array and use the while loop
-# to iterate over each member of that array, yielding each element contained in
+# 
+# You'll be building out the content of the #my_each method. 
+# This method should accept an argument of an array and use the while loop 
+# to iterate over each member of that array, yielding each element contained in 
 # the array to a block.
-#
+# 
 # Here's an example of what should happen when you call your my_each method:
