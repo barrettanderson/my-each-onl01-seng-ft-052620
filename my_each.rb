820895e7ab0930bf08d005073dc8f813b72d87e6
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-29 17:27:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,4 @@
-def my_each(file) # put argument(s) here
+def my_each(words) # put argument(s) here
   # loops through an array
   # does something here with the elements of an array
   # returns the original array
